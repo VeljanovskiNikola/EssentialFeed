c6a2675781561b0ea0e6e840e942a7c31d009c75
Nikola <veljanovskin92@gmail.com>
2024-10-28 14:46:33 +0100

    Replace `RemoteFeedLoader.Result` enum with the pre defined `LoadFeedResult` enum from the Feed module (in preparation to conform to the `FeedLoader` protocol

--- EssentialFeed/Feed API/FeedItemsMapper.swift
+++ EssentialFeed/Feed API/FeedItemsMapper.swift
@@ -15,7 +15,7 @@ internal final class FeedItemsMapper {
             return items.map { $0.item }
         }
     }
-
+    
     private struct Item: Decodable {
         let id: UUID
         let description: String?
@@ -30,16 +30,8 @@ internal final class FeedItemsMapper {
         }
     }
     
-   private static var OK_200: Int { return 200 }
-
-   internal static func map(_ data: Data, _ response: HTTPURLResponse) throws -> [FeedItem] {
-        guard response.statusCode == OK_200 else {
-            throw RemoteFeedLoader.Error.invalidData
-        }
-        return try JSONDecoder().decode(Root.self,
-                                        from: data).items.map { $0.item }
-    }
-
+    private static var OK_200: Int { return 200 }
+    
     internal static func map(_ data: Data, from response: HTTPURLResponse) -> RemoteFeedLoader.Result {
         guard response.statusCode == OK_200,
               let root = try? JSONDecoder().decode(Root.self, from: data) else {
