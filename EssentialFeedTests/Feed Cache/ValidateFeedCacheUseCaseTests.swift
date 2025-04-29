0b78e7d21aff7e4464c7f4b05b68242fc6afebda
Nikola <veljanovskin92@gmail.com>
2025-04-29 23:16:31 +0200

    Extract duplicate test helpers into a shared scope

--- EssentialFeedTests/Feed Cache/ValidateFeedCacheUseCaseTests.swift
+++ EssentialFeedTests/Feed Cache/ValidateFeedCacheUseCaseTests.swift
@@ -33,7 +33,7 @@ class ValidateFeedCacheUseCaseTests: XCTestCase {
         
         XCTAssertEqual(store.receivedMessages, [.retrieve])
     }
-
+    
     func test_validateCache_doesNotDeleteLessThanSevenDaysOldCache() {
         let feed = uniqueImageFeed()
         let fixedCurrentDate = Date()
@@ -45,7 +45,7 @@ class ValidateFeedCacheUseCaseTests: XCTestCase {
         
         XCTAssertEqual(store.receivedMessages, [.retrieve])
     }
-
+    
     // MARK: - Helpers
     
     private func makeSUT(currentDate: @escaping () -> Date = Date.init,
@@ -57,53 +57,5 @@ class ValidateFeedCacheUseCaseTests: XCTestCase {
         trackForMemoryLeaks(store, file: file, line: line)
         trackForMemoryLeaks(sut, file: file, line: line)
         return (sut, store)
-    }
-        
-    private func uniqueImage() -> FeedImage {
-        return FeedImage(id: UUID(),
-                         description: "any",
-                         location: "any",
-                         url: anyURL())
-    }
-    
-    private func uniqueImageFeed() -> (
-        models: [FeedImage],
-        local: [LocalFeedImage]
-    ) {
-        let models = [
-            uniqueImage(),
-            uniqueImage()
-        ]
-        let local = models.map { LocalFeedImage(
-            id: $0.id,
-            description: $0.description,
-            location: $0.location,
-            url: $0.url
-        )}
-        return (models, local)
-    }
-    
-    private func anyURL() -> URL {
-        URL(string: "http://any-url.com")!
-    }
-    
-    private func anyNSError() -> NSError {
-        NSError(domain: "any error", code: 0)
-    }
-}
-
-private extension Date {
-    func adding(days: Int) -> Date {
-        return Calendar(
-            identifier: .gregorian
-        ).date(
-            byAdding: .day,
-            value: days,
-            to: self
-        )!
-    }
-    
-    func adding(seconds: TimeInterval) -> Date {
-        return self + seconds
     }
 }
---
+++ EssentialFeedTests/Helpers/SharedTestHelpers.swift
@@ -0,0 +1,16 @@
+//
+//  SharedTestHelpers.swift
+//  EssentialFeedTests
+//
+//  Created by Nikola Veljanovski on 29.4.25.
+//
+
+import Foundation
+
+func anyNSError() -> NSError {
+    NSError(domain: "any error", code: 0)
+}
+
+func anyURL() -> URL {
+    URL(string: "http://any-url.com")!
+}
