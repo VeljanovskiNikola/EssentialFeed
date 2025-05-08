1988e0bbe8eddd249ea00e16582e927387af2f82
Nikola <veljanovskin92@gmail.com>
2025-05-08 21:39:29 +0200

    Add `EssentialFeedCacheIntegrationTests` target to separate the potentially-slower cache integation tests from the fat unit/isolated tests

---
+++ EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
@@ -0,0 +1,13 @@
+//
+//  EssentialFeedCacheIntegrationTests.swift
+//  EssentialFeedCacheIntegrationTests
+//
+//  Created by Nikola Veljanovski on 8.5.25.
+//
+
+import XCTest
+
+final class EssentialFeedCacheIntegrationTests: XCTestCase {
+
+    
+}
