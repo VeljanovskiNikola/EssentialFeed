bc5f158fe2435829850fd96a089e86358b02fe2a
Nikola <veljanovskin92@gmail.com>
2025-07-05 08:18:58 +0200

    Replace closure event handler with delegate Protocol to demonstrate different composition approaches

--- EssentialFeediOS/Feed UI/Controllers/FeedRefreshViewController.swift
+++ EssentialFeediOS/Feed UI/Controllers/FeedRefreshViewController.swift
@@ -7,12 +7,15 @@
 
 import UIKit
 
+protocol FeedRefreshViewControllerDelegate {
+    func didRequestFeedRefresh()
+}
+
 public final class FeedRefreshViewController: NSObject, FeedLoadingView {
+    private let delegate: FeedRefreshViewControllerDelegate
     
-    private let loadFeed: () -> Void
-    
-    init(loadFeed: @escaping () -> Void) {
-        self.loadFeed = loadFeed
+    init(delegate: FeedRefreshViewControllerDelegate) {
+        self.delegate = delegate
     }
     
     public lazy var view: UIRefreshControl = {
@@ -20,7 +23,7 @@ public final class FeedRefreshViewController: NSObject, FeedLoadingView {
     }()
     
     @objc func refresh() {
-        loadFeed()
+        delegate.didRequestFeedRefresh()
     }
     
     func display(_ viewModel: FeedLoadingViewModel) {
