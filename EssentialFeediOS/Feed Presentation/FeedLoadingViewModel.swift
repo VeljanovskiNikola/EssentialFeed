63271ee182e03afd3b683793924cf5cece5e8bf9
Nikola <veljanovskin92@gmail.com>
2025-07-05 10:56:30 +0200

    Move View Models to separate files

---
+++ EssentialFeediOS/Feed Presentation/FeedLoadingViewModel.swift
@@ -0,0 +1,11 @@
+//
+//  FeedLoadingViewModel.swift
+//  EssentialFeed
+//
+//  Created by Nikola Veljanovski on 5.7.25.
+//
+
+struct FeedLoadingViewModel {
+    let isLoading: Bool
+}
+
---
+++ EssentialFeediOS/Feed Presentation/FeedViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  FeedViewModel.swift
+//  EssentialFeed
+//
+//  Created by Nikola Veljanovski on 5.7.25.
+//
+
+import EssentialFeed
+
+struct FeedViewModel {
+    let feed: [FeedImage]
+}
+
